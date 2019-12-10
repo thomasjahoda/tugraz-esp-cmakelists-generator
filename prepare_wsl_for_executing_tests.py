8c3f7c880d62e6f47d699f4a6629b8d8625084de
Thomas Jahoda <thomas.jahoda@gmail.com>
2019-12-10 22:59:16 +0100

    improve error-handling in prepare_wsl_for_executing_tests.py

--- prepare_wsl_for_executing_tests.py
+++ prepare_wsl_for_executing_tests.py
@@ -1,6 +1,7 @@
 import sys
 
 from fabric import Connection
+from invoke import UnexpectedExit
 
 if len(sys.argv) != 2:
     print("usage: [SSH connection string]")
@@ -12,9 +13,9 @@ ssh_connection = Connection(ssh_connection_string)
 ssh_connection.run('echo "=== START OF WSL-SIDE COMMANDS/OUTPUT ==="')
 try:
     ssh_connection.run('sudo echo sudotest')
-except:
+except UnexpectedExit as e:
     raise Exception("please configure passwordless sudo (is effort, is big no-no)"
-                    " or copy the SSH commands from the code manually (recommended)")
+                    " or copy the SSH commands from the code manually (recommended)") from e
 ssh_connection.run('sudo apt update')
 ssh_connection.run('sudo apt install clang make xclip git ctags valgrind python3 python3-pip -y')
 ssh_connection.run('sudo pip3 install --upgrade pexpect pyaml diff-match-patch')
